3a6ee1ac2eda63908d1153735b0eb8945ddd6e09
pdas9647 <padmanabhadas9647@gmail.com>
2025-07-11 22:33:31 +0530

    Add config.ts

---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+export const PORT = 20253;
+export const DB_NAME = "github";
+export const CLIENT_ID = ""
+export const CLIENT_SECRET = ""
+export const GITHUB_CALLBACK_URL = `http://localhost:${PORT}/github/oauth/callback`
+export const MONGODB_URI = ""
+export const TOKEN_SECRET = ""
